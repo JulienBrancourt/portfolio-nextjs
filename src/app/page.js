05aac6bd68f02d60d873181ed51ef39914f3557c
ElyanisJ <brancourt.julien@hotmail.fr>
2023-11-10 17:16:02 +0100

    revision architecture

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,52 @@
 "use client"
+import React, { useState } from "react";
 // import Link from "next/link"
 import Image from "next/image";
-import ModalButton from "./components/ModalButton"
-import { useState } from "react";
+// import ModalButton from "./components/ModalButton"
+import ProjectCard from "./components/ProjectCard";
+import ModalContent from "./components/ModalContent"
+// import { useState } from "react";
 
 export default function Home() {
 
+    const ProjectList = () => {
+    const projects = [
+      { id: 1, name: 'booki', details: 'un projet top' }, 
+      { id: 2, name: 'nounou', details: 'un projet top aussi' }
+    ];
+
+    const [selectedProject, setSelectedProject] = useState(null);
+
+    const handleClick = (projectId) => {
+      setSelectedProject(projects.find((project) => project.id === projectId));
+    };
+
+    // N'oubliez pas de retourner quelque chose
+    return (
+      <ul className="card-list">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <ProjectCard
+              projectId={project.id}
+              projectName={project.name}
+              onClick={handleClick}
+            />
+          </li>
+        ))}
+
+        {selectedProject && (
+          <li key={selectedProject.id}>
+            {/* Affichez les détails du projet dans votre modale ici */}
+            <Modal>
+              <h1>{selectedProject.name}</h1>
+              <p>{selectedProject.details}</p>
+            </Modal>
+          </li>
+        )}
+      </ul>
+    );
+    };
+  
   return (
     <main>
       {/* <Link href="/">Home</Link>
@@ -47,8 +88,8 @@ export default function Home() {
       </section>
 
       <section className="projets">
-        <ul class="card-list">
-          <li class="card">
+        {/*<ul class="card-list">
+           <li class="card">
             <Image
               class="clipped-img"
               src="/assets/bali.jpg"
@@ -79,9 +120,8 @@ export default function Home() {
                 <ModalButton/>
               </div>
             </div>
-          </li>
-          
-        </ul>
+          </li> */}
+          <ProjectList />
       </section>
        
     </main>
