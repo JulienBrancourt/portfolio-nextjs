bcdf286ac29d04db10167da442dd5fd072021733
ElyanisJ <brancourt.julien@hotmail.fr>
2023-11-17 10:07:49 +0100

    positionnement modale et modif btn close

--- src/app/components/Modal/Modal.js
+++ src/app/components/Modal/Modal.js
@@ -5,18 +5,25 @@ const Modal = ({name, projetPic, handleClose, tech, description}) => {
   return (
     <div className="modal-backdrop">
 
-        <div className="modal">
-            <img className="modal-img" src={projetPic} alt="projet pic" />
-            <div className="modal-body">
-               <div className='modal-name'>{name}</div>
-               <div className='modal-description'>{description}</div>
-               <div className="skills">
-               <ul>
-                    {tech.map(tech => <li key={tech}>{tech}</li>)}
-               </ul>
-               </div>
+      <div className="modal">
+        <div class="container-close" onClick={() => handleClose()}>  
+          <div class="circle">
+          <div class="line first"></div>
+          <div class="line second"></div>
+          </div>                
+        </div>
+        <img className="modal-img" src={projetPic} alt="projet pic" />
+        <div className="modal-body">
+            <div className='modal-name'>{name}</div>
+            <div className='modal-description'>{description}</div>
+            <div className="skills">
+            <ul>
+                {tech.map(tech => <li key={tech}>{tech}</li>)}
+            </ul>
             </div>
-            <button className="close-btn" onClick={()=> handleClose()}>x</button>
+        </div>
+          {/* <button className="close-btn" onClick={()=> handleClose()}>x</button> */}
+          
         </div>
 
     </div>
