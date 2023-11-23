7ede92b5652717df59d56768fe96f12a9074796d
ElyanisJ <brancourt.julien@hotmail.fr>
2023-11-23 18:01:19 +0100

    suppr g analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import Head from 'next/head'
+import Head from 'next/head';
 import './globals.css';
-
-
 
 export const metadata = {
   title: 'Julien Brancourt | Portfolio',
   description: 'Julien Brancourt, développeur web, développeur front-end React & Next.js',
-  
-}
+};
 
 export default function RootLayout({ children }) {
-
   const gtmScript = `
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-T8WZRZBM');
-  `; 
-
-  const gtagScript = `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-N0M6MV1453');
   `;
 
   return (
@@ -38,8 +27,6 @@ export default function RootLayout({ children }) {
         <meta property="og:description" content="Portfolio de Julien Brancourt, développeur web, développeur front-end React & Next.js" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N0M6MV1453"></script>
-        <script dangerouslySetInnerHTML={{ __html: gtagScript }} />
       </Head>
       <body>
         {children}
@@ -50,5 +37,6 @@ export default function RootLayout({ children }) {
         </noscript>
       </body>
     </html>
-  )
+  );
 }
+
